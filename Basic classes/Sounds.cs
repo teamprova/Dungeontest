03e4f866a086286dfaf6632ebd217ee9210abe72
ArthurCose <ArthurCose@gmail.com>
2015-12-09 15:00:41 -0500

    Added missing Sounds.cs

---
+++ Basic classes/Sounds.cs
@@ -0,0 +1,26 @@
+﻿using System.IO;
+using Microsoft.Xna.Framework.Audio;
+
+namespace DungeonTest
+{
+	public class Sounds
+	{
+        SoundEffect sound;
+
+        public Sounds(string src)
+        {
+            using (FileStream stream = File.Open(src, FileMode.Open))
+            {
+                sound = SoundEffect.FromStream(stream);
+                stream.Close();
+            }
+        }
+
+		// Play a Sound
+		public void Play(float volume, float pan)
+		{
+            sound.Play(volume, 1, pan);
+		}
+	}
+}
+
